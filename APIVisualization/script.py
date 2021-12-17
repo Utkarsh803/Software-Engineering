d7a87b73499b7a715f5059369497012b05e599a4
Utkarsh Gupta <guptau@tcd.ie>
2021-12-17 22:59:02 +0000

    Added basic API extraction code

--- APIVisualization/script.py
+++ APIVisualization/script.py
@@ -1,2 +1,19 @@
-# a script to do phython based access to the github api
-print("Demonstration of API based on python");
+#import Github from the pyGithub library
+from github import Github
+
+# using an access token--create own token to use
+g = Github(" ")
+
+#getting basic user data
+usr = g.get_user()
+print("user:  " + usr.login)
+print("fullname:  " + usr.name)
+
+#getting repositories of the user
+repos = g.get_user().get_repos()
+
+for repo in repos:
+    print(repo.name)
+
+
+
